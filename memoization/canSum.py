624ba91d25bffa736d6baaa14117744d6cc276a1
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2023-08-17 08:52:26 -0400

    cansuM

---
+++ memoization/canSum.py
@@ -0,0 +1,15 @@
+hasmap = {}
+def canSum(target, numbers):
+    if target in hasmap:
+        return hasmap[target]
+    if target == 0 :
+        return True
+    if target<0:
+        return False
+    for x in numbers:
+        if canSum(target-x,numbers):
+            return True
+    hasmap[target] = False
+    return False
+a = canSum(300,[7, 14])
+print(a)
