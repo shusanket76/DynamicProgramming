1d209428802f62cf245853b9de05e4b9ae83cf83
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2023-10-22 00:51:24 -0400

    Fib

---
+++ memoization/fib.py
@@ -0,0 +1,12 @@
+hasmap = {}
+def fib(n):
+    if n in hasmap:
+        return hasmap[n]
+    if n<=2:
+        return 1
+    hasmap[n]=fib(n-1)+fib(n-2)
+    return hasmap[n]
+
+print(fib(12))
+print(fib(13))
+print(fib(133))
